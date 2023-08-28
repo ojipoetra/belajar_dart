263be3ebd3a0c84cf50ac950529e40ddda35609e
ojipoetra <ojishefals2@gmail.com>
2023-08-28 13:58:14 +0700

    memasukan variable

--- bin/tugas_praktek3.dart
+++ bin/tugas_praktek3.dart
@@ -1,5 +1,5 @@
 class TugasPraktek3 {
-  logikaStatment() {
+  soalPertama() {
     int nilai = 70;
 
     if (nilai > 70) {
@@ -13,16 +13,28 @@ class TugasPraktek3 {
     }
   }
 
-  perulangan() {
+  soalKedua() {
     for (var i = 1; i <= 10; i++) {
       print(i);
     }
   }
+
+  void soalKetiga(List<int> lisData, int pengali) {
+    List<int> listBaru = [];
+    Future.delayed(Duration(seconds: 2), () {
+      for (var el in lisData) {
+        int hitung = el * pengali;
+        print("perkalian $pengali * $el =  $hitung");
+        listBaru.add(hitung);
+        print(listBaru);
+      }
+    });
+  }
 }
 
-void main() {
+void main() async {
   var tugas = TugasPraktek3();
-
-  tugas.logikaStatment();
-  tugas.perulangan();
+  tugas.soalKetiga([1, 2, 3, 4], 3);
+  tugas.soalPertama();
+  tugas.soalKedua();
 }
